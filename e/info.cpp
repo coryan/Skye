1bc0b77e11b58854df8534cbcde10e3d32e88374
coryan <coryan@fd57b2e0-0160-48e9-a97c-3d3d408012f7>
2014-01-11 16:28:16 +0000

    SMF-30: These files are not part of Skye

--- e/info.cpp
+++
@@ -1,7 +0,0 @@
-#include <e/info.hpp>
-#include <e/e-config.hpp>
-
-std::string e::version()
-{
-  return std::string(VERSION);
-}
--- e/job_execution_request.cpp
+++
@@ -1,7 +0,0 @@
-#include <e/job_execution_request.hpp>
-#include <iostream>
-
-std::ostream & e::operator<<(
-    std::ostream & os, job_execution_request const & x) {
-  return os << "request.id=" << x.id() << ", request.spec=" << x.job();
-}
